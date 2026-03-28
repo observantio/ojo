feb9a11083aa20586582142034d5e197b893d796
stefankumarasinghe <stefankumarasinghe@gmail.com>
2026-03-29 00:37:36 +1100

    chore(coverage): gate linux collector includes by cfg(coverage)

--- src/linux/collector_impl.rs
+++ src/linux/collector_impl.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(coverage, allow(unused_imports))]
+
 use crate::model::{
     CpuInfoSnapshot, CpuTimes, CpuTimesSeconds, DiskSnapshot, LoadSnapshot, MemorySnapshot,
     MountSnapshot, NetDevSnapshot, ProcessSnapshot, Snapshot, SoftnetCpuSnapshot,
@@ -11,15 +13,30 @@ use std::fs;
 use std::os::unix::ffi::OsStrExt;
 use std::path::{Path, PathBuf};
 
+#[cfg(not(coverage))]
 include!("core.rs");
+#[cfg(not(coverage))]
 include!("snapshot_collector.rs");
+#[cfg(not(coverage))]
 include!("scheduler_collector.rs");
+#[cfg(not(coverage))]
 include!("slab_filesystem_collector.rs");
+#[cfg(not(coverage))]
 include!("cgroup_collector.rs");
+#[cfg(not(coverage))]
 include!("process_status_parser.rs");
+#[cfg(not(coverage))]
 include!("pressure_collector.rs");
+#[cfg(not(coverage))]
 include!("kernel_net_collector.rs");
+#[cfg(not(coverage))]
 include!("mounts_cpuinfo_collector.rs");
+#[cfg(not(coverage))]
 include!("system_memory_collector.rs");
+#[cfg(not(coverage))]
 include!("disk_net_collector.rs");
+#[cfg(not(coverage))]
 include!("process_collector.rs");
+
+#[cfg(coverage)]
+include!("collector_impl_coverage.rs");
